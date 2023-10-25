1ca9e55325f6a143d27c960be0edfe033e9abe30
Vinicius <vini456nascimento@gmail.com>
2023-10-25 08:28:59 -0300

    fix: serializer courses

--- courses/serializers.py
+++ courses/serializers.py
@@ -2,12 +2,13 @@ from rest_framework import serializers
 from .models import Courses
 from contents.serializers import ContentSerializer
 from students_courses.serializers import StudentCourseSerializer
+from accounts.models import Account
 
 
 class CoursesSerializer(serializers.ModelSerializer):
     contents = ContentSerializer(many=True, read_only=True)
     students_courses = StudentCourseSerializer(many=True, read_only=True)
-    instructor_id = serializers.UUIDField(required=False)
+    instructor = serializers.UUIDField(source="instructor_id", required=False)
 
     class Meta:
         model = Courses
@@ -17,14 +18,16 @@ class CoursesSerializer(serializers.ModelSerializer):
             "status",
             "start_date",
             "end_date",
-            "instructor_id",
+            "instructor",
             "contents",
             "students_courses",
         )
 
     def create(self, validated_data):
-        instructor = validated_data.get("instructor_id")
-        if instructor is None:
+        instructor_id = validated_data.get("instructor")
+        if instructor_id is None:
             instructor = self.context["request"].user
-            validated_data["instructor_id"] = instructor
+        else:
+            instructor = Account.objects.get(id=instructor_id)
+        validated_data["instructor"] = instructor
         return Courses.objects.create(**validated_data)
