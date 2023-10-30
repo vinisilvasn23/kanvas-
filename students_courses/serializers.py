103760fd81c0e40c25dd9f7644493376bb1b5fbe
Vinicius <vini456nascimento@gmail.com>
2023-10-30 10:03:07 -0300

    feat: update serializer student

--- students_courses/serializers.py
+++ students_courses/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
-from .models import Course
+from .models import StudentCourse
 
 
-class StudentCourseSerializer(serializers.ModelSerializer):
+class StudentSerializer(serializers.ModelSerializer):
+    student_id = serializers.UUIDField(source="student.id", required=False)
+    student_username = serializers.CharField(source="student.username", required=False)
+    student_email = serializers.EmailField(source="student.email", read_only=True)
+
     class Meta:
-        model = Course
-        fields = '__all__'
+        model = StudentCourse
+        fields = (
+            "id",
+            "student_id",
+            "student_username",
+            "student_email",
+            "status",
+        )
