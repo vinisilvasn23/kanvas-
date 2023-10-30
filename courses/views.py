fdc9dfd7274ef5ef1adf487fdb75c9cf96d9e369
Vinicius <vini456nascimento@gmail.com>
2023-10-30 10:00:45 -0300

    feat: update courses view

--- courses/views.py
+++ courses/views.py
@@ -1,31 +1,37 @@
 from .models import Course
-from .serializers import CoursesSerializer
+from .serializers import CoursesSerializer, CourseListSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated
-from .permissions import IsSuperuserOrReadOnly, IsSuperUserOrUser
+from .permissions import IsSuperuserOrReadOnly
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.exceptions import NotFound
 
 
 class ListCreateCoursesView(ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated, IsSuperuserOrReadOnly]
+    permission_classes = [IsSuperuserOrReadOnly]
 
     serializer_class = CoursesSerializer
 
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return CourseListSerializer
+        return super().get_serializer_class()
+
     def get_queryset(self):
         user = self.request.user
-        queryset = Course.objects.all()
-
-        if not user.is_superuser:
-            queryset = queryset.filter(instructor_id=user.id)
+        if user.is_superuser:
+            queryset = Course.objects.all()
+        else:
+            queryset = Course.objects.filter(
+                students=user
+                )
 
         return queryset
 
 
 class RetrieveUpdateDestroyCoursesView(RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsSuperUserOrUser]
+    permission_classes = [IsSuperuserOrReadOnly]
     serializer_class = CoursesSerializer
     queryset = Course.objects.all()
     lookup_url_kwarg = "course_id"
@@ -34,6 +40,6 @@ class RetrieveUpdateDestroyCoursesView(RetrieveUpdateDestroyAPIView):
         course = Course.objects.filter(pk=self.kwargs["course_id"]).first()
 
         if not course:
-            raise NotFound({"detail": "course not found."})
+            raise NotFound({"detail": "content not found."})
 
         return course
