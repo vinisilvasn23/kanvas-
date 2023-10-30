d3ef6482e0dc3ec3fd34cb488824518c4cb53b91
Vinicius <vini456nascimento@gmail.com>
2023-10-30 10:00:26 -0300

    feat: adding routes to courses

--- courses/urls.py
+++ courses/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from contents.views import CreateContentView, ListUpdateDestroyContentView
+from students_courses.views import AddStudentToCourseView
 
 urlpatterns = [
     path(
@@ -9,5 +11,17 @@ urlpatterns = [
     path(
         "courses/<uuid:course_id>/",
         views.RetrieveUpdateDestroyCoursesView.as_view()
+        ),
+    path(
+        "courses/<uuid:course_id>/contents/",
+        CreateContentView.as_view()
+        ),
+    path(
+        "courses/<uuid:course_id>/contents/<uuid:content_id>/",
+        ListUpdateDestroyContentView.as_view()
+        ),
+    path(
+        "courses/<uuid:course_id>/students/",
+        AddStudentToCourseView.as_view()
         ),
 ]
