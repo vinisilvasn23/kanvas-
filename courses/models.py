15c8c023f81c66ea4def1eafc641aaf625c27830
Vinicius <vini456nascimento@gmail.com>
2023-10-30 09:59:01 -0300

    feat: update model courses

--- courses/models.py
+++ courses/models.py
@@ -13,13 +13,20 @@ class Course(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
     name = models.CharField(max_length=100, unique=True, null=False)
     status = models.CharField(
-        max_length=20,
+        max_length=11,
         choices=CourseStatusEnum.choices,
-        default=CourseStatusEnum.NOT_STARTED
-        )
+        default=CourseStatusEnum.NOT_STARTED,
+    )
     start_date = models.DateField(null=False)
     end_date = models.DateField(null=False)
     instructor = models.ForeignKey(
-        Account, on_delete=models.CASCADE,
-        related_name="courses"
-        )
+        Account,
+        on_delete=models.CASCADE,
+        related_name="courses",
+        null=True,
+        blank=True
+    )
+    students = models.ManyToManyField(
+        Account,
+        related_name="my_courses"
+    )
