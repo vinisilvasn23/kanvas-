d99257d2308c5efbc6205e5011b07bdafa0138f4
Vinicius <vini456nascimento@gmail.com>
2023-10-30 12:43:37 -0300

    feat: implementing url doc

--- _core/urls.py
+++ _core/urls.py
@@ -16,9 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("accounts.urls")),
-    path("api/", include("courses.urls"))
+    path("api/", include("courses.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(),
+    ),
 ]
