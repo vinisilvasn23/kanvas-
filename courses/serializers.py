f92ebdc350099f3d05354acb157519580a4aade3
Vinicius <vini456nascimento@gmail.com>
2023-10-30 09:59:42 -0300

    feat: update serializer courses

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 from .models import Course
 from contents.serializers import ContentSerializer
-from students_courses.serializers import StudentCourseSerializer
+from students_courses.serializers import StudentSerializer
 from accounts.models import Account
 
 
 class CoursesSerializer(serializers.ModelSerializer):
     contents = ContentSerializer(many=True, read_only=True)
-    students_courses = StudentCourseSerializer(many=True, read_only=True)
+    students_courses = StudentSerializer(many=True, read_only=True)
     instructor = serializers.UUIDField(source="instructor_id", required=False)
 
     class Meta:
@@ -24,17 +24,42 @@ class CoursesSerializer(serializers.ModelSerializer):
         )
 
     def create(self, validated_data):
-        instructor_id = validated_data.get("instructor")
-        if instructor_id is None:
-            instructor = self.context["request"].user
-        else:
+        instructor_id = validated_data.get("instructor_id")
+        if instructor_id:
             instructor = Account.objects.get(id=instructor_id)
-        validated_data["instructor"] = instructor
+            validated_data["instructor"] = instructor
         return Course.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
-
         instance.save()
         return instance
+
+
+class CourseListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = (
+            "id",
+            "name",
+            "status",
+            "start_date",
+            "end_date",
+            "instructor",
+            "contents",
+            "students_courses",
+        )
+
+
+class StudentCourseSerializer(serializers.ModelSerializer):
+    students_courses = StudentSerializer(many=True, read_only=True)
+    name = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = Course
+        fields = (
+            "id",
+            "name",
+            "students_courses",
+        )
