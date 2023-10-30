3d7ad2d530be0b0572089f02decf91fc06fd9d59
Vinicius <vini456nascimento@gmail.com>
2023-10-30 12:44:08 -0300

    feat: add extend_schema accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,13 @@
 from .models import Account
 from .serializers import AccountSerializer
 from rest_framework.generics import CreateAPIView
+from drf_spectacular.utils import extend_schema
 
 
+@extend_schema(
+    description="Rota para criação de contas",
+    tags=["Criação de usuário"],
+)
 class CreateAccountView(CreateAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
