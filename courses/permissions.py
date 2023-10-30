f4bb83ba5bb1e1fa699c5dff115b08c944c8b0e1
Vinicius <vini456nascimento@gmail.com>
2023-10-30 09:59:27 -0300

    feat: update permissions courses

--- courses/permissions.py
+++ courses/permissions.py
@@ -6,12 +6,3 @@ class IsSuperuserOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return request.user.is_authenticated
         return request.user.is_authenticated and request.user.is_superuser
-
-
-class IsSuperUserOrUser(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-
-        if request.user.is_superuser:
-            return True
-
-        return request.user.students_courses.filter(course=obj).exists()
