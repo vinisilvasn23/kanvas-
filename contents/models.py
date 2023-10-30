50877aa9b6d1e84eb9b6d7026573d14e94fd7820
Vinicius <vini456nascimento@gmail.com>
2023-10-30 09:57:22 -0300

    feat: update content model

--- contents/models.py
+++ contents/models.py
@@ -7,7 +7,7 @@ class Content(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
     name = models.CharField(max_length=150, null=False)
     content = models.TextField(null=False)
-    video_url = models.CharField(max_length=200)
+    video_url = models.CharField(max_length=200, blank=True)
     course = models.ForeignKey(
         Course, null=False,
         on_delete=models.CASCADE,
